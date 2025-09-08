852fdf77c0cf4f42e242f67f524cf5864ab830c9
Luan Vieira <luanvlw23@gmail.com>
2025-09-08 19:16:32 -0300

    Configuração inicial do projeto Django com view de tarefas

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_tarefas, name='lista_tarefas'),
-    path('exercicio-post/', views.exercicio_post_view, name='exercicio_post'),
+    path("", views.index, name="index"),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,19 +1,4 @@
-from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Tarefa
 
-def lista_tarefas(request):
-    if request.method == 'POST':
-        novo_titulo = request.POST.get('titulo')
-        Tarefa.objects.create(titulo=novo_titulo)
-        return redirect('lista_tarefas')
-
-    tarefas = Tarefa.objects.all().order_by('concluida')
-    contexto = {'tarefas': tarefas}
-    return render(request, 'tarefas/lista.html', contexto)
-
-def exercicio_post_view(request):
-    if request.method == 'POST':
-        return HttpResponse("Hello world")
-
-    return render(request, 'tarefas/exercicio_post.html')
+def index(request):
+    return HttpResponse("Página inicial de tarefas 🚀")
