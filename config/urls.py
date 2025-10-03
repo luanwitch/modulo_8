447e7e342fa0fb45267f148a7d6feaa434555c51
Luan Vieira <luanvwittchind@gmail.com>
2025-10-03 09:37:59 -0300

    Configura rota raiz e adiciona requirements.txt

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+from turtle import home
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tarefas.urls")), 
+    path("", home, name="home"),
 ]
